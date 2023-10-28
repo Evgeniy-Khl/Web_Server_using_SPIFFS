593bb39086830dffde49580b4040a6a69f024511
Evgeniy-Khl <graviton.ua@gmail.com>
2023-10-28 12:27:12 +0300

    new webServer & switch.html

--- Web_Server_using_SPIFFS.ino
+++ Web_Server_using_SPIFFS.ino
@@ -47,48 +47,52 @@ String ledState;
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-String getTemperature() {
-  temperature = temperature + 0.1; //bme.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float temperature = 1.8 * bme.readTemperature() + 32;
+//*****************functions***********************************
+String GPIO_status(int numGPIO){
+  String state;
+  if (!digitalRead(numGPIO)) state = "-OFF";
+  else state = "-ON";
+  Serial.println(state);
+  return String(state);
+}
+
+String GPIO_status_inv(int numGPIO){
+  String state;
+  if (digitalRead(numGPIO)) state = "-OFF";
+  else state = "-ON";
+  Serial.println(state);
+  return String(state);
+}
+
+String getTemperature(){
+  temperature = temperature + 0.1;
   Serial.println(temperature);
   return String(temperature);
 }
   
 String getHumidity() {
-  humidity = humidity + 0.1; //bme.readHumidity();
+  humidity = humidity + 0.1;
   Serial.println(humidity);
   return String(humidity);
 }
 
 String getPressure() {
-  pressure = pressure - 0.1; //bme.readPressure()/ 100.0F;
+  pressure = pressure - 0.1;
   Serial.println(pressure);
   return String(pressure);
 }
 
-// Replaces placeholder with LED state value
 String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(ledPin)){
-      ledState = "ON";
-    }
-    else{
-      ledState = "OFF";
-    }
-    Serial.print(ledState);
-    return ledState;
-  }
-  else if (var == "TEMPERATURE"){
-    return getTemperature();
-  }
-  else if (var == "HUMIDITY"){
-    return getHumidity();
-  }
-  else if (var == "PRESSURE"){
-    return getPressure();
-  }
+  Serial.print(var);
+  if(var == "STATE2") return GPIO_status_inv(2);
+  else if(var == "STATE5") return GPIO_status(5);
+  else if(var == "STATE4") return GPIO_status(4);
+  else if(var == "STATE14") return GPIO_status(14);
+  else if(var == "STATE12") return GPIO_status(12);
+  else if(var == "STATE13") return GPIO_status(13);
+  else if (var == "TEMPERATURE") return getTemperature();
+  else if (var == "HUMIDITY") return getHumidity();
+  else if (var == "PRESSURE") return getPressure();
   return String();
 }
 
@@ -111,26 +115,18 @@ String relayState(int numRelay){
   }
   return "";
 }
- 
+//************************************************************** 
+
 void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
+  Serial.begin(115200);				// Serial port for debugging purposes
   pinMode(ledPin, OUTPUT);
 
-  // Initialize the sensor
- // if (!bme.begin(0x76)) {
- //   Serial.println("Could not find a valid BME280 sensor, check wiring!");
- //   while (1);
- // }
-
-  // Initialize SPIFFS
-  if(!SPIFFS.begin()){
+  if(!SPIFFS.begin()){				// Initialize SPIFFS
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
   
-  // Set all relays to off when the program starts - if set to Normally Open (NO), the relay is off when you set the relay to HIGH
-  for(int i=1; i<=NUM_RELAYS; i++){
+  for(int i=1; i<=NUM_RELAYS; i++){	// Set all relays to off when the program starts - if set to Normally Open (NO), the relay is off when you set the relay to HIGH
     pinMode(relayGPIOs[i-1], OUTPUT);
     if(RELAY_NO){
       digitalWrite(relayGPIOs[i-1], HIGH);
@@ -140,52 +136,50 @@ void setup(){
     }
   }
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);		// Connect to Wi-Fi
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
 
-  ftpSrv.begin("relay", "relay");     // Піднімаємо FTP-сервер для зручності налагодження роботи HTML (логін: relay, пароль: relay)
+  ftpSrv.begin("relay", "relay");	// Піднімаємо FTP-сервер для зручності налагодження роботи HTML (логін: relay, пароль: relay)
 
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());	// Print ESP32 Local IP Address
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){			// Route for ROOT / web page
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
   
-  // Route for SETUP1 / web page
-  server.on("/setup", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/setup", HTTP_GET, [](AsyncWebServerRequest *request){		// Route for SETUP / web page
     request->send(SPIFFS, "/setup.html", String(), false, processor);
   });
   
-  // Route for SETUP2 / web page
-  server.on("/switch", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/switch", HTTP_GET, [](AsyncWebServerRequest *request){	// Route for SWITCH / web page
     request->send(SPIFFS, "/switch.html", String(), false, processor);
   });
-  
-  // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+
+//**************** SWITCH .html *******************************************************************************************  
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {	// Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
     String inputMessage;
     String inputParam;
     String inputMessage2;
     String inputParam2;
-    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
+    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>     ("GET", "/update?relay=5&state=1")
     if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
-      inputMessage = request->getParam(PARAM_INPUT_1)->value();
-      inputParam = PARAM_INPUT_1;
-      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      inputParam2 = PARAM_INPUT_2;
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();		// "rel4"
+      inputMessage = inputMessage.substring(3);						// "4"
+	  Serial.println(inputMessage);
+      inputParam = PARAM_INPUT_1;			// "relay"
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();	// "1"
+	  Serial.println(inputMessage2);
+      inputParam2 = PARAM_INPUT_2;			// "state"
       if(RELAY_NO){
         Serial.print("NO ");
-        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());	// установка вывода в значение
       }
       else{
         Serial.print("NC ");
-        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
       }
     }
     else {
@@ -195,7 +189,33 @@ void setup(){
     Serial.println(inputMessage + inputMessage2);
     request->send(200, "text/plain", "OK");
   });
+  
+   server.on("/status", HTTP_GET, [] (AsyncWebServerRequest *request) {	// Send a GET request to <ESP_IP>/status?rel=<inputMessage>
+    String inputMessage;
+    String inputParam;
 
+    // GET input1 value on <ESP_IP>/status?relay=<inputMessage>     ("GET", "/status?relay=5")
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();		// "5"
+	  Serial.println(inputMessage);
+      inputParam = PARAM_INPUT_1;			// "relay"
+      if(RELAY_NO){
+        Serial.print("NO ");
+		request->send_P(200, "text/plain", GPIO_status(relayGPIOs[inputMessage.toInt()-1]).c_str());
+      }
+      else{
+        Serial.print("NC ");
+		request->send_P(200, "text/plain", GPIO_status_inv(relayGPIOs[inputMessage.toInt()-1]).c_str());
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  }); 
+//******************************************************************************************************************************
   // Route to set GPIO to HIGH
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(ledPin, HIGH);    
@@ -219,6 +239,10 @@ void setup(){
   server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getPressure().c_str());
   });
+  
+ // server.on("/GPIO14_status", HTTP_GET, [](AsyncWebServerRequest *request){
+ //   request->send_P(200, "text/plain", GPIO_status(14).c_str());
+ // });
 
   // Start server
   server.begin();
